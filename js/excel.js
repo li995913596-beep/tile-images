2d90aa11bbd93768e279ba1e66e73c798cce08e1
li995913596-beep <li995913596@gmail.com>
2026-02-28 13:01:32 +0700

    Update excel.js

--- js/excel.js
+++ js/excel.js
@@ -6,6 +6,13 @@ import {
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+function safeId(text) {
+  return text
+    .replace(/\//g, "_")     // 替换 /
+    .replace(/\s+/g, "")     // 去空格
+    .replace(/#/g, "")       // 去 #
+}
+
 export async function importExcel(file) {
 
   const data = await file.arrayBuffer();
@@ -26,7 +33,12 @@ export async function importExcel(file) {
 
       if (!code || !spec || !color || !warehouse) continue;
 
-      const docId = `${code}_${color}_${warehouse}`;
+      const safeCode = safeId(code);
+      const safeColor = safeId(color);
+      const safeWarehouse = safeId(warehouse);
+
+      const docId = `${safeCode}_${safeColor}_${safeWarehouse}`;
+
       const ref = doc(db, "inventory", docId);
       const snap = await getDoc(ref);
 
