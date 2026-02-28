895c7bb2146e1b31fb501ca8228a8f291e61a557
li995913596-beep <li995913596@gmail.com>
2026-02-28 10:33:22 +0700

    Update and rename excel.service.js to excel.js

--- js/excel.js
+++ js/excel.js
@@ -1,15 +1,12 @@
 import { db } from "./firebase.js";
-import {
-collection,
-doc,
-writeBatch,
-getDocs
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, doc, writeBatch, getDocs }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 export async function importExcel(file){
 
 const inventoryRef=collection(db,"inventory");
 const batch=writeBatch(db);
+
 const data=await file.arrayBuffer();
 const wb=XLSX.read(data);
 
@@ -21,26 +18,25 @@ const sheet=wb.Sheets[sheetName];
 const json=XLSX.utils.sheet_to_json(sheet);
 
 for(let row of json){
-
 const code=String(row["编号"]).trim();
 if(!code) continue;
+
+const safeCode = code.replaceAll("/", "-");
 
 const stockValue=Number(row["库存"]||0);
 const reservedValue=Number(row["留货(库存已扣)"]||0);
 const realStock=stockValue+reservedValue;
 
-batch.set(doc(db,"inventory",`${code}_${sheetName}`),{
+batch.set(doc(db,"inventory",`${safeCode}_${sheetName}`),{
 code,
 spec:row["规格"]||"",
 color:row["色号"]||"",
 warehouse:sheetName,
 stock:realStock,
-reserved:reservedValue,
-updatedAt:new Date()
+reserved:reservedValue
 });
 }
 }
 
 await batch.commit();
-alert("导入完成");
 }
