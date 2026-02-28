a4aa2cd54522dcbf36d71d81d0012217178988a6
li995913596-beep <li995913596@gmail.com>
2026-02-28 10:31:47 +0700

    Update firebase.js configuration and add auth export
    Removed storageBucket and messagingSenderId from firebaseConfig and added auth export.

--- js/firebase.js
+++ js/firebase.js
@@ -6,12 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyD5QmrAYVacp-HsPWh4n4mkXmruGATTZ8o",
   authDomain: "kucun-2cb51.firebaseapp.com",
   projectId: "kucun-2cb51",
-  storageBucket: "kucun-2cb51.firebasestorage.app",
-  messagingSenderId: "170723608901",
-  appId: "1:170723608901:web:943af1d44f8c3fda52d62c",
-  measurementId: "G-GVSBHL4V02"
+
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
 export const auth = getAuth(app);
