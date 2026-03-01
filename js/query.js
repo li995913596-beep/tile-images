510452b67af50a123420f14b1518c48b14cc5cd2
li995913596-beep <li995913596@gmail.com>
2026-03-01 11:42:42 +0700

    Update query.js

--- js/query.js
+++ js/query.js
@@ -51,7 +51,7 @@ async function searchData(){
     return;
   }
 
-  // 表头（保持你原结构）
+  // 表头（电脑模式使用）
   resultDiv.innerHTML=`
     <div class="table-header">
       <div>图片<br>Image</div>
@@ -76,24 +76,27 @@ async function searchData(){
       <div class="table-row">
 
         <div class="img-col"
-          data-label="Image"
+          data-label="图片 Image"
           onclick="openModal('${imageUrl}')">
           <img src="${imageUrl}"
             loading="lazy"
             onerror="this.style.display='none'">
         </div>
 
-        <div data-label="Code">${item.code}</div>
-        <div data-label="Size">${item.spec||"-"}</div>
-        <div data-label="Color">${item.color||"-"}</div>
+        <div data-label="编号 Code">${item.code}</div>
 
-        <div data-label="Stock"
+        <div data-label="规格 Size">${item.spec||"-"}</div>
+
+        <div data-label="色号 Color">${item.color||"-"}</div>
+
+        <div data-label="数量 Stock"
              class="${lowStock?'low-stock':''}">
           ${item.stock}
         </div>
 
-        <div data-label="Warehouse">${item.warehouse||"-"}</div>
-        <div data-label="Reserved">${item.reserved}</div>
+        <div data-label="仓库 Warehouse">${item.warehouse||"-"}</div>
+
+        <div data-label="留货 Reserved">${item.reserved}</div>
 
       </div>
     `;
