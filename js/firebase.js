0250367c6f47af9a30ed6fdd1642e74429644290
li995913596-beep <li995913596@gmail.com>
2026-08-01 14:58:00 +0800

    feat: Firebase Storage image upload + prefer cloud images

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDuhQD3bhkFmvZ2OIfJldYwtznX_2PVtIk",
   authDomain: "kucunguanli-13d73.firebaseapp.com",
   projectId: "kucunguanli-13d73",
-
+  storageBucket: "kucunguanli-13d73.appspot.com",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
 export const auth = getAuth(app);
-
+export const storage = getStorage(app);
