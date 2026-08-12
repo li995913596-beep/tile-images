382862e000b2837495c7e3ab39d8bb7710a7c30a
li995913596-beep <li995913596@gmail.com>
2026-08-12 10:48:55 +0800

    feat: load admin_reserve_time for hold date tracking

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,4 @@
 console.log("admin.js 开始执行");
 await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@946e1b1774977319700f5f38f86808c45921f283/js/admin.js");
 await import("./admin_pack.js?v=20260806b");
+await import("./admin_reserve_time.js?v=20260812a");
