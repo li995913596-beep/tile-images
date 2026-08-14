8271f55dd7f62a144f2b806c5370940a1bcdeaca
li995913596-beep <li995913596@gmail.com>
2026-08-14 16:28:25 +0800

    fix: load ship_plan.js instead of broken admin_ship_plan

--- js/admin.js
+++ js/admin.js
@@ -2,4 +2,4 @@ console.log("admin.js 开始执行");
 await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@946e1b1774977319700f5f38f86808c45921f283/js/admin.js");
 await import("./admin_pack.js?v=20260806b");
 await import("./admin_reserve_time.js?v=20260812a");
-await import("./admin_ship_plan.js?v=20260814c");
+await import("./ship_plan.js?v=20260814d");
