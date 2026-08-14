95b1766b9ba0d6ce3a4f9f4e93d4ff7c177fb6ff
li995913596-beep <li995913596@gmail.com>
2026-08-14 17:12:27 +0800

    chore: pin ship_plan with 留货/可出

--- js/admin.js
+++ js/admin.js
@@ -2,4 +2,4 @@ console.log("admin.js 开始执行");
 await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@946e1b1774977319700f5f38f86808c45921f283/js/admin.js");
 await import("./admin_pack.js?v=20260806b");
 await import("./admin_reserve_time.js?v=20260812a");
-await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@bb277400d05f410ab043a80bbe6a6c7f98f7f370/js/ship_plan.js");
+await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@201b31cfc9a8fcd1b0f1ed6e9bd2f8a89598f9bf/js/ship_plan.js");
