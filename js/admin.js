22512c76fa2cce3411005b437840596edbffca25
li995913596-beep <li995913596@gmail.com>
2026-08-15 10:11:17 +0800

    fix: load out_from_plan after admin core

--- js/admin.js
+++ js/admin.js
@@ -2,4 +2,5 @@ console.log("admin.js 开始执行");
 await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@946e1b1774977319700f5f38f86808c45921f283/js/admin.js");
 await import("./admin_pack.js?v=20260806b");
 await import("./admin_reserve_time.js?v=20260812a");
-await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@201b31cfc9a8fcd1b0f1ed6e9bd2f8a89598f9bf/js/ship_plan.js");
+await import("./ship_plan.js?v=20260815a");
+await import("./out_from_plan.js?v=20260815b");
