665ef8a7d9b13781ac5249d129ea9eabb8a2198b
li995913596-beep <li995913596@gmail.com>
2026-08-15 10:22:04 +0800

    chore: load out_from_plan v20260815c

--- js/admin.js
+++ js/admin.js
@@ -3,4 +3,4 @@ await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@946e1b177
 await import("./admin_pack.js?v=20260806b");
 await import("./admin_reserve_time.js?v=20260812a");
 await import("./ship_plan.js?v=20260815a");
-await import("./out_from_plan.js?v=20260815b");
+await import("./out_from_plan.js?v=20260815c");
