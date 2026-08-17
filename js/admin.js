5b861d90e28b235bcbbe7fc4e78932cdad73ea65
li995913596-beep <li995913596@gmail.com>
2026-08-17 11:31:32 +0800

    fix: restore admin.js loader + ship_plan v20260817b

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,9 @@
-PLACEHOLDER4
+console.log("admin.js 开始执行");
+await import("https://cdn.jsdelivr.net/gh/li995913596-beep/tile-images@946e1b1774977319700f5f38f86808c45921f283/js/admin.js");
+await import("./admin_pack.js?v=20260806b");
+await import("./admin_reserve_time.js?v=20260812a");
+await import("./ship_plan.js?v=20260817b");
+await import("./out_from_plan.js?v=20260815g");
+await import("./admin_log_tools.js?v=20260815g");
+await import("./boxes.js?v=20260815j");
+await import("./grout.js?v=20260817a");
